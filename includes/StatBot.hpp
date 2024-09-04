b234cd228407f4117578fd61b5d14b683823fb51
Reda96R <redarayyad@yahoo.com>
2024-09-04 12:37:50 +0100

    cleaning the code

--- includes/StatBot.hpp
+++ includes/StatBot.hpp
@@ -2,11 +2,16 @@
 # define STATBOT_HPP
 
 # include <map>
+# include <vector>
 # include <iostream>
+
+class Channel;
 
 class statBot{
 	public:
-		std::map<std::string, int>	  messagesCount;
+		void									  ircBot( Channel& channel );
+		std::map<std::string, int>				  messagesCount;
+		std::vector<std::pair<std::string, int> > getStats( Channel& channel );
 
 };
 
--- srcs/server/statBot.cpp
+++ srcs/server/statBot.cpp
@@ -1,13 +1,13 @@
 #include "../../includes/Helpers.hpp"
 
 // Comparator for sorting the vector of pairs in descending order of counts
-bool comparePairs(const std::pair<std::string, int>& a, const std::pair<std::string, int>& b) {
-    return a.second > b.second;
+bool comparePairs(const std::pair<std::string, int>& a, const std::pair<std::string, int>& b){
+    return (a.second > b.second);
 }
 
-std::vector<std::pair<std::string, int> > getTopContributors( Channel& channel ) {
-	// Convert the map to a vector of pairs
+std::vector<std::pair<std::string, int> > getStats( Channel& channel ){
 	size_t	  totalmessages = 0;
+	// Convert the map to a vector of pairs
     std::vector<std::pair<std::string, int> > contributors;
 	std::map<std::string, int> messagesCount = channel.statBot.messagesCount; 
     for (std::map<std::string, int>::iterator it = messagesCount.begin(); it != messagesCount.end(); ++it){
@@ -20,11 +20,12 @@ std::vector<std::pair<std::string, int> > getTopContributors( Channel& channel )
 
    // Add the header as the first element
     std::vector<std::pair<std::string, int> > result;
-    result.push_back(std::make_pair(":::: Channel Age :::", -1)); // -1 signifies header
+    result.push_back(std::make_pair("::::::: Channel Stats ::::::", -1)); // -1 signifies header
+    result.push_back(std::make_pair("~~~~ Channel Age ~~~", -1));
     result.push_back(std::make_pair(channel.getChannelAge(), -1)); 
-    result.push_back(std::make_pair(":::: Totale Messages :::", -1));
+    result.push_back(std::make_pair("~~~~ Totale Messages ~~~", -1));
     result.push_back(std::make_pair(intToString(totalmessages), -1));
-    result.push_back(std::make_pair(":::: Messages Leaderboard :::", -1));
+    result.push_back(std::make_pair("~~~~ Messages Leaderboard ~~~", -1));
 
     // Add the sorted contributors with rankings
     for (size_t i = 0; i < contributors.size(); ++i) {
@@ -39,10 +40,7 @@ std::vector<std::pair<std::string, int> > getTopContributors( Channel& channel )
 
 void	ircBot( Channel& channel ){
 
-	std::vector<std::pair<std::string, int> >	data = getTopContributors(channel);
-	// data.push_back("StatBot reporting for duty!");
-	// data.push_back("test1");
-	// data.push_back("test2");
+	std::vector<std::pair<std::string, int> > data = getStats(channel);
 
 	for (size_t i = 0; i < data.size(); ++i){
 		std::string message = ":statBot PRIVMSG " + channel.getChannelName() + " " + data[i].first;
